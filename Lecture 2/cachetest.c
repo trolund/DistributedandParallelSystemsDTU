8d8bce37fc373cd1b05d9ec8cb18da245911da66
trolund@gmail.com <vxh77kdp>
2019-02-14 17:10:02 +0100

    Lecture 2

---
+++ Lecture 2/cachetest.c
@@ -0,0 +1,101 @@
+/*  COPYING:
+ *  Dave Patterson at UCB modified this code from a program by Andrea 
+ *  Dusseau of U.C. Berkeley,which was based on a description in 
+ *  Saavedra-Barrera [1992]:
+ *    Saavedra-Barrera, R. H. [1992]. CPU Performance Evaluation and 
+ *    Execution Time Prediction Using Narrow Spectrum Benchmarking, 
+ *    Ph.D. Dissertation, University of Calif., Berkeley (May).
+ *  Patterson has given permission to use and modify this code as long
+ *  as these people and the U. of Cal., Berkeley get proper credit.
+ */
+
+#include <stdio.h>
+#include <sys/times.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define CACHE_MIN  (1024)         /* smallest cache */
+#define CACHE_MAX  (32*1024*1024) /* largest cache */
+#define STRIDE_MAX (256)          /* largest stride (array indices) */
+
+#define SAMPLE     (1000000)      /* to get reasonably large time sample */
+#define TIME_MAX   (0.5)          /* measurement time (seconds) */
+
+#define CLK_TCK    (100)          /* number of system clock ticks per second */
+
+
+int x[CACHE_MAX]; /* array going to stride through */
+ 
+double get_seconds() { /* routine to read time */
+  struct tms rusage;
+  times(&rusage);    /* UNIX utility: time in clock ticks */
+  return (double) (rusage.tms_utime)/CLK_TCK;
+}
+
+double get_secondsXXX() { 
+  /* Get current time using clock_gettime using CLOCK_MONOTONIC */
+  return 0.0;
+}
+
+int main() {
+  int register i, stride;
+  int register * ptr, * limit;
+  int register dummy;
+  int register steps, tsteps;
+  unsigned char register b1, b2;     /* comparison flags */
+  int csize;
+  double register sec0, sec1, sec2;  /* timing variables */
+
+  /* Check for proper clock tick constant definition */
+  int sys_clock_tick = sysconf(_SC_CLK_TCK);
+  if (CLK_TCK != sys_clock_tick) {
+    printf("CLK_TCK = %d differs from system clock tick = %d\n", 
+           CLK_TCK, sys_clock_tick);
+    printf("Define CLK_TCK to (%d) and run again\n", sys_clock_tick);  
+    exit(1);
+  }
+
+  for (csize=CACHE_MIN; csize <= CACHE_MAX; csize=csize*2) 
+    for (stride=1; stride <= STRIDE_MAX; stride=stride*2) {
+
+      limit = &x[csize-1]; /* last element of current cache size */
+     
+      steps = tsteps = 0;
+      ptr = x;
+      sec0 = get_seconds();            // get start time 
+      do {
+        for (i=0; i < SAMPLE; i++) {
+          if (ptr > limit) ptr = x;    // reset pointer if end reached 
+          *ptr = *ptr + 1;             // Access to memory/cache 
+          ptr = ptr + stride;
+        }
+        steps = steps + 1;             // count while loop iterations
+        sec1 = get_seconds() - sec0;   // measure time
+        b1 = sec1 < TIME_MAX;
+        b2 = tsteps < steps;
+      } while (b1);
+     
+      /* Repeat empty loop to measure overhead */
+      ptr = x;
+      sec0 = get_seconds();            // get start time 
+      do {
+          for (i=0; i < SAMPLE; i++) {
+          if (ptr > limit) ptr = x;    // reset pointer if end reached
+          dummy = dummy + 1;           // Dummy access to register
+          ptr = ptr + stride;
+        }
+        tsteps = tsteps + 1;           // count while loop iterations
+        sec2 = get_seconds() - sec0;   // measure time
+        b1 = sec2 < TIME_MAX;
+        b2 = tsteps < steps;
+      } while (b2);
+     
+      printf("Size (bytes):%9d, Stride (bytes):%4d, "
+             "read+write:%6.2f ns  (steps: %3d)\n",
+             csize*sizeof(int), stride*sizeof(int),
+             (double) (sec1-sec2)*1e9/(steps*SAMPLE), steps);
+
+    }; /* end of both outer for loops */
+}
---
+++ Lecture 2/matrix_sum.c
@@ -0,0 +1,80 @@
+/* matrix_sum.c 
+
+   02346, F19, DTU
+
+   For instructions, see Databar Exercise 2.
+*/
+
+#include <stdio.h>
+#include <time.h>
+
+#define ROWS  (8*1024)
+#define COLS  (8*1024)
+
+long matrix[ROWS][COLS];
+
+double get_seconds() { 
+  /* Use a suitable clock reading function here */
+  return 0.0;
+}
+
+
+void fill(long m[ROWS][COLS]) {
+  int i,j;
+
+  for (i=0; i < ROWS; i++) {
+      for (j=0; j < COLS; j++) {
+        m[i][j] = i+j; 
+      }
+  }
+}
+
+long sum1(long m[ROWS][COLS]) {
+  long register sum = 0;
+  int i,j;
+
+  for (i=0; i < ROWS; i++) {
+    for (j=0; j < COLS; j++) {
+      sum += m[i][j];
+    }
+  }
+  return sum;
+}
+
+long sum2(long m[ROWS][COLS]) {
+  long register sum = 0;
+  int i,j;
+
+  for (j=0; j < COLS; j++) {
+    for (i=0; i < ROWS; i++) {
+      sum += m[i][j];
+    }
+  }
+  
+  return sum;
+}
+
+
+int main() {
+
+  int sum;
+  double start, end;
+
+  fill(matrix);
+
+  printf("Matrix size: %dk x %dk = %dM\n", 
+	 ROWS/1024, COLS/1024, ROWS*COLS/(1024*1024));
+
+  start = get_seconds();
+  sum = sum1(matrix);
+  end = get_seconds();
+
+  printf("sum1(matrix) = %ld using %1.6fs\n", sum, end - start);
+
+  start = get_seconds();
+  sum = sum2(matrix);
+  end = get_seconds();
+
+  printf("sum2(matrix) = %ld using %1.6fs\n", sum, end - start);
+
+}
